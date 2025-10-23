88073e5e65a8057448db2704c905349bdbf47087
Amandeep Singh <amandeepaluna1@gmail.com>
2025-10-23 21:44:21 +0530

    single api, 3 types user

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -6,13 +6,15 @@ import {redis} from '../config/redis.js'
 import 'dotenv/config'
 import crypto from 'crypto'
 import bcrypt from 'bcryptjs';
+import College from '../models/College.js';
+import Teacher from '../models/Teacher.js';
 
 // signup controller
 
 export async function signup(req,res){
-    const {fullName, email, password} = req.body;
+    const {role, fullName, email, password} = req.body;
     try{
-        if(!fullName || !email || !password){
+        if(!role || !fullName || !email || !password){
             return res.status(400).json({
                 success: false,
                 message: "All fields are required"
@@ -25,26 +27,52 @@ export async function signup(req,res){
                 message: "Fill correct email"
             })
         }
-        const existingUser = await Student.findOne({email});
+        let existingUser;
+        if(role === 'student') existingUser = await Student.findOne({email});
+        else if(role === 'teacher') existingUser = await Teacher.findOne({email});
+        else if(role === 'college') existingUser = await College.findOne({email});
+        else return res.status(400).json({
+            success: false,
+            message: "No valid role"
+        })
         if(existingUser){
             return res.status(400).json({
+                success: false,
                 message: "User email already exists"
             })
         }
         const idx = Math.floor(Math.random()*100)+1;
         const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
 
-        const newStudent = await Student.create({
-            email, fullName, password, profilePic: randomAvatar
-        })
+        // const newStudent = await Student.create({
+        //     email, fullName, password, profilePic: randomAvatar
+        // })
+        let newUser;
+        if (role === 'student') {
+            newUser = await Student.create({ email, fullName, password, profilePic: randomAvatar });
+        } else if (role === 'teacher') {
+            newUser = await Teacher.create({ email, fullName, password, profilePic: randomAvatar });
+        } else if (role === 'college') {
+            newUser = await College.create({ email, fullName, password, profilePic: randomAvatar });
+        }
 
-        const token = jwt.sign(
-            {userId: newStudent._id,}, process.env.JWT_SECRET_KEY, {expiresIn: '7d'}
-        )
+
+        // const token = jwt.sign(
+        //     {userId: newStudent._id, role : "student"}, process.env.JWT_SECRET_KEY, {expiresIn: '7d'}
+        // )
+
+        const token = jwt.sign({ userId: newUser._id, role }, process.env.JWT_SECRET_KEY, { expiresIn: '7d' });
 
         res.cookie("jwt", token,{
             maxAge: 7*24*60*60*1000,
             httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        res.cookie("role",role,{
+            httpOnly: true,
+            maxAge: 7*24*60*60*1000,
             sameSite: "strict",
             secure: process.env.NODE_ENV === "production"
         })
@@ -55,10 +83,10 @@ export async function signup(req,res){
         })
     }
     catch(err){
-        console.log("Error in signup controller" + err.message);
+        console.log("Error in signup controller " + err.message);
         res.status(500).json({
             success: false,
-            message: "Error in signup controller" + err.message
+            message: "Error in signup controller " + err.message
         })
     }
 }
@@ -68,14 +96,22 @@ export async function signup(req,res){
 
 export async function login(req,res){
     try{
-        const {email, password} = req.body;
-        if(!email || !password) {
+        const {role, email, password} = req.body;
+        if(!role || !email || !password) {
             return res.status(400).json({
-                sucess: false,
+                success: false,
                 message: "All fields are required"
             })
         }
-        const user = await Student.findOne({email}).select("-password");
+        let user;
+        if(role === 'student') user = await Student.findOne({email});
+        else if(role === 'college') user = await College.findOne({email});
+        else if(role === 'teacher') user = await Teacher.findOne({email});
+        else return res.status(400).json({
+            success: false,
+            message: "No valid role"
+        })
+        
         if(!user) return res.status(401).json({
             success: false,
             message: "Invalid credentials"
@@ -86,25 +122,46 @@ export async function login(req,res){
             message: "Invalid credentials"
         })
 
-        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET_KEY, {expiresIn: '7d'});
+        const token = jwt.sign({userId: user._id, role : 'student'}, process.env.JWT_SECRET_KEY, {expiresIn: '7d'});
 
-        res.cookie("jwt",token,{
-            httpOnly: true,
-            maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
-            secure: process.env.NODE_ENV === "production"
-        })
+        // res.cookie("jwt",token,{
+        //     httpOnly: true,
+        //     maxAge: 7*24*60*60*1000,
+        //     sameSite: "strict",
+        //     secure: process.env.NODE_ENV === "production"
+        // })
 
-        res.cookie("email",email,{
+        // res.cookie("email",email,{
+        //     httpOnly: true,
+        //     maxAge: 7*24*60*60*1000,
+        //     sameSite: "strict",
+        //     secure: process.env.NODE_ENV === "production"
+        // })
+        
+        // res.cookie("role",role,{
+        //     httpOnly: true,
+        //     maxAge: 7*24*60*60*1000,
+        //     sameSite: "strict",
+        //     secure: process.env.NODE_ENV === "production"
+        // })
+
+        function setCookie(res, key, value) {
+        res.cookie(key, value, {
             httpOnly: true,
-            maxAge: 7*24*60*60*1000,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
             sameSite: "strict",
-            secure: process.env.NODE_ENV === "production"
-        })        
+            secure: process.env.NODE_ENV === "production",
+        });
+        }
+
+        setCookie(res, "jwt", token);
+        setCookie(res, "email", email);
+        setCookie(res, "role", role);
+
         res.status(201).json({
             sucess: true,
             message: "Login successfully",
-            user: user
+            // user: user
         })
     }
     catch(err){
@@ -163,6 +220,7 @@ export async function studentOnboard (req, res) {
         })
         return res.status(202).json({
             success: true,
+            message: "Successfully Onboard",
             user: update
         })
     }
@@ -186,7 +244,17 @@ export async  function sendOtp(req, res){
             success: false,
             message: "Email is required"
         })
-        const user = await Student.findOne({email});
+        let user = await Student.findOne({ email });
+        let schema = "student";
+        if (!user) {
+        user = await College.findOne({ email });
+        schema = "college"
+        }
+
+        if (!user) {
+        user = await Teacher.findOne({ email });
+        schema = 'teacher'
+        }
         if(!user) return res.status(400).json({
             success: false,
             message: "Invalid credentials"
@@ -204,7 +272,8 @@ export async  function sendOtp(req, res){
         }
         const otp = Math.floor(100000 + Math.random()*900000).toString();
 
-        await Promise.all([await redis.del(`otp:${email}`),
+        await Promise.all([
+         redis.del(`otp:${email}`),
          redis.set(`email:${otp}`, email, {ex:300}),
          redis.set(`otp:${email}`, otp, {ex: 300}),
          redis.set(cooldownKey, '1', { ex: 60 }),])
@@ -217,6 +286,12 @@ export async  function sendOtp(req, res){
             html: `<h3>Your OTP: ${otp}</h3><p>Valid for 5 minutes.</p>`
         })
         
+        res.cookie("schema",schema,{
+            httpOnly: true,
+            maxAge: 5*60*1000,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production"
+        })
         res.status(200).json({
             success: true,
             message: "Otp send successfully"
@@ -239,7 +314,14 @@ export async function verifyOtp (req,res) {
         })
 
         const email = await redis.get(`email:${otp}`);
-        const user = await Student.findOne({email});
+        // const user = await Student.findOne({email});
+        const [student, college, teacher] = await Promise.all([
+            Student.findOne({ email }),
+            College.findOne({ email }),
+            Teacher.findOne({ email }),
+        ]);
+        const user = student || college || teacher;
+
 
         if(!user) return res.status(400).json({
             success: false,
@@ -251,19 +333,28 @@ export async function verifyOtp (req,res) {
             success: false,
             message: "Please enter correct otp"
         })
-
+        const schema = req.cookies.schema;
         user.isOtpVerified = true;
         // await user.save()  // Load full doc, modify, validate entire doc, save
 
 
         // by below method => Update only specified fields in DB
-        await Student.updateOne(
-        { email },
-        { $set: { isOtpVerified: true } }
-        );
-
+        if(schema === 'student'){
+            await Student.updateOne(
+            { email },
+            { $set: { isOtpVerified: true } });
+        }else if(schema === 'college'){
+            await College.updateOne(
+            { email },
+            { $set: { isOtpVerified: true } });
+        }else if(schema === 'teacher'){
+            await Teacher.updateOne(
+            { email },
+            { $set: { isOtpVerified: true } });
+        }
         await redis.del(`otp:${email}`);
         await redis.del(`email:${otp}`);
+        
 
         
 
@@ -294,6 +385,7 @@ export async function resetPassword (req, res) {
     try{
     const { enterPassword, confirmPassword} = req.body;
     const email = req.cookies.email
+    const schema = req.cookies.schema
     if(!enterPassword || !confirmPassword) return res.status(401).json({
         success: false,
         message: "Please enter all fields"
@@ -306,7 +398,15 @@ export async function resetPassword (req, res) {
     if (!email) {
     return res.status(400).json({ message: "Invalid or expired reset token" });
     }
-    const user = await Student.findOne({email});
+    // const user = await Student.findOne({email});
+    const [student, college, teacher] = await Promise.all([
+        Student.findOne({ email }),
+        College.findOne({ email }),
+        Teacher.findOne({ email }),
+    ]);
+
+    const user = student || college || teacher;
+
     if(!user) return res.status(400).json({
         success: false,
         message: "Invalid!, no user find"
@@ -318,18 +418,45 @@ export async function resetPassword (req, res) {
     })
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(enterPassword, salt);
-
-    await Student.updateOne(
-        {email},
-        {
-            $set: {
-                password: hashPassword,
-                isOtpVerified:false
+    if(schema === 'student'){
+        await Student.updateOne(
+            {email},
+            {
+                $set: {
+                    password: hashPassword,
+                    isOtpVerified:false
+                }
+                
             }
-            
-        }
-    )
-
+        )
+    }else if(schema === 'college'){
+            await College.updateOne(
+            {email},
+            {
+                $set: {
+                    password: hashPassword,
+                    isOtpVerified:false
+                }
+                
+            }
+        )
+    }else if(schema === 'teacher'){
+            await Teacher.updateOne(
+            {email},
+            {
+                $set: {
+                    password: hashPassword,
+                    isOtpVerified:false
+                }
+                
+            }
+        )
+    }else{
+        return res.status(400).json({
+            success: false,
+            message: "No or invalid schema"
+        })
+    }
     
     res.status(201).json({
         success: true,
