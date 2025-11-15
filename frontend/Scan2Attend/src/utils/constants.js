b02a0d23a9f170e7daa7378f479e9073e1c38adb
Amandeep Singh <amandeepaluna1@gmail.com>
2025-11-15 21:44:51 +0530

    test

--- frontend/Scan2Attend/src/utils/constants.js
+++ frontend/Scan2Attend/src/utils/constants.js
@@ -1,2 +1,10 @@
-export const backendUrl = location.hostname === 'localhost'? import.meta.env.VITE_BACKEND_URL : "https://scan2attend-backend.onrender.com";
-export const frontendUrl = location.hostname === 'localhost'? import.meta.env.VITE_FRONTEND_URL : "https://scan2attend.onrender.com";
+// export const backendUrl = location.hostname === 'localhost'? import.meta.env.VITE_BACKEND_URL : "https://scan2attend-backend.onrender.com";
+// export const frontendUrl = location.hostname === 'localhost'? import.meta.env.VITE_FRONTEND_URL : "https://scan2attend.onrender.com";
+
+export const backendUrl = location.hostname === "localhost"
+  ? "http://localhost:3001/api"
+  : "https://scan2attend-backend.onrender.com";
+
+export const frontendUrl = location.hostname === "localhost"
+  ? "http://localhost:5173"
+  : "https://scan2attend.onrender.com";
