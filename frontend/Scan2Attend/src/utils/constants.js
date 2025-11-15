46b7c2b69db843f8e581e57d1d584f8687246713
Amandeep Singh <amandeepaluna1@gmail.com>
2025-11-15 21:53:18 +0530

    test

--- frontend/Scan2Attend/src/utils/constants.js
+++ frontend/Scan2Attend/src/utils/constants.js
@@ -3,7 +3,7 @@
 
 export const backendUrl = location.hostname === "localhost"
   ? "http://localhost:3001/api"
-  : "https://scan2attend-backend.onrender.com";
+  : "https://scan2attend-backend.onrender.com/api";
 
 export const frontendUrl = location.hostname === "localhost"
   ? "http://localhost:5173"
