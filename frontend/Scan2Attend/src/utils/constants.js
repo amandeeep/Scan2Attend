196cc88d193acbaaa35b6e3a0e3134826fd4b430
Amandeep Singh <amandeepaluna1@gmail.com>
2025-11-15 21:01:25 +0530

    add backend urll

--- frontend/Scan2Attend/src/utils/constants.js
+++ frontend/Scan2Attend/src/utils/constants.js
@@ -1,3 +1,3 @@
 
-export const backendUrl = location.hostname === 'localhost'? import.meta.env.BACKEND_URL : "";
+export const backendUrl = location.hostname === 'localhost'? import.meta.env.BACKEND_URL : "https://scan2attend-backend.onrender.com";
 export const frontendUrl = location.hostname === 'localhost'? import.meta.env.FRONTEND_URL : "";
